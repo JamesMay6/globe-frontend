9fd39e88acbda9deeb0323725557bfe8359f886e
James Mason <j91mason6@gmail.com>
2025-05-30 22:35:49 +0100

    update globe viewer

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,24 @@ function App() {
   useEffect(() => {
     Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ION_TOKEN;
 
-    const viewer = new Viewer("cesiumContainer", {
-      terrainProvider: new CesiumTerrainProvider({
-        url: IonResource.fromAssetId(1),
-      }),
-      imageryProvider: new UrlTemplateImageryProvider({
-        url: "https://assets.agi.com/stk-terrain/world/stk-terrain-4tiles/{z}/{x}/{y}.png",
-        maximumLevel: 13,
-      }),
-      geocoder: true,
-      baseLayerPicker: false,
-      timeline: false,
-      animation: false,
-      fullscreenButton: false,
-      vrButton: false,
-      homeButton: false,
-      sceneModePicker: false,
-      navigationHelpButton: false,
-      infoBox: false,
-      selectionIndicator: false,
-    });
+    const viewer = async () => {
+          const terrainProvider = await Cesium.createWorldTerrainAsync();
+          const viewer = new Cesium.Viewer(container, {
+            terrainProvider,
+            imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+              url: "https://a.tile.openstreetmap.org/",
+            }),
+            animation: false,
+            timeline: false,
+            homeButton: false,
+            sceneModePicker: false,
+            navigationHelpButton: false,
+            requestRenderMode: true,
+            maximumRenderTimeChange: 0,
+            contextOptions: { requestWebgl2: true },
+          });
+        };
+    
 
     viewer.camera.setView({
       destination: Cartesian3.fromDegrees(-74.0, 40.7, 10000),
