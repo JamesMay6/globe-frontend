e65a6020f59466ed42f8b908af923dca430171f4
James Mason <j91mason6@gmail.com>
2025-05-30 22:22:12 +0100

    updted globe

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import { useEffect } from "react";
-import {
-  Viewer,
-  CesiumTerrainProvider,
-  Ion,
-  IonResource,
-  Geocoder,
-  ScreenSpaceEventHandler,
-  ScreenSpaceEventType,
-} from "cesium";
+import { Viewer, CesiumTerrainProvider, Ion, IonResource } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 
 function App() {
@@ -18,6 +10,7 @@ function App() {
       terrainProvider: new CesiumTerrainProvider({
         url: IonResource.fromAssetId(1),
       }),
+      geocoder: true,          // enable built-in search box
       baseLayerPicker: false,
       timeline: false,
       animation: false,
@@ -28,17 +21,9 @@ function App() {
       navigationHelpButton: false,
       infoBox: false,
       selectionIndicator: false,
-      geocoder: false, // we'll add it manually
     });
 
-    // Add Geocoder widget manually to control its position and styling
-    const geocoder = new Geocoder({
-      container: document.getElementById("geocoderContainer"),
-      viewer: viewer,
-      scene: viewer.scene,
-    });
-
-    // Ensure globe is shown (disable any globe hiding)
+    // Ensure globe and imagery are visible
     viewer.scene.globe.show = true;
 
     return () => {
@@ -46,28 +31,7 @@ function App() {
     };
   }, []);
 
-  return (
-    <>
-      {/* Cesium Container */}
-      <div id="cesiumContainer" style={{ width: "100vw", height: "100vh" }} />
-
-      {/* Geocoder container positioned over Cesium container */}
-      <div
-        id="geocoderContainer"
-        style={{
-          position: "absolute",
-          top: "10px",
-          left: "10px",
-          zIndex: 1,
-          width: "300px",
-          backgroundColor: "white",
-          padding: "5px",
-          borderRadius: "4px",
-          boxShadow: "0 2px 6px rgba(0,0,0,0.3)",
-        }}
-      />
-    </>
-  );
+  return <div id="cesiumContainer" style={{ width: "100vw", height: "100vh" }} />;
 }
 
 export default App;
