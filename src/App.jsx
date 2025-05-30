9317ffb86c2c1e3ecbadd1341ac35bc5a307037e
James Mason <j91mason6@gmail.com>
2025-05-30 21:17:48 +0100

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import 'cesium/Build/Cesium/Widgets/widgets.css';
 
 function App() {
   useEffect(() => {
-    import('cesium').then((Cesium) => {
+    import('cesium').then((module) => {
+      const Cesium = module.default; // ✅ FIX: grab from `.default`
+
       const viewer = new Cesium.Viewer('cesiumContainer', {
         terrainProvider: Cesium.createWorldTerrain(),
         baseLayerPicker: false,
