19c3aae9cb9a6c0371c571f96428c970a0b1eb48
James Mason <j91mason6@gmail.com>
2025-05-30 22:06:57 +0100

    added token

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useEffect } from 'react';
-import { Viewer, createWorldTerrain } from 'cesium';
+import { Viewer, CesiumTerrainProvider, Ion, IonResource } from 'cesium';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
 
 function App() {
   useEffect(() => {
+    // Set your Cesium Ion access token here
+    Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxYWY2NGY4YS1kY2UxLTQ5OTItODczNy1kYmFmM2VjZGVlM2MiLCJpZCI6MzAxODU3LCJpYXQiOjE3NDcwODMxNzR9.P0nAM3Q4S1yQvWEmKSgo1l2nmUvJd9_xrnLL1ZeM43Q";
+    
+
     const viewer = new Viewer('cesiumContainer', {
-      terrainProvider: createWorldTerrain(),
+      terrainProvider: new CesiumTerrainProvider({
+        url: IonResource.fromAssetId(1), // World Terrain asset ID on Cesium Ion
+      }),
       baseLayerPicker: false,
     });
 
