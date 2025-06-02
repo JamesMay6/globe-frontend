a426e274c7ce9a1344d7e25616c0388ea029ece0
James Mason <j91mason6@gmail.com>
2025-06-02 16:10:31 +0100

    T&Cs update

--- src/App.jsx
+++ src/App.jsx
@@ -456,12 +456,8 @@ useEffect(() => {
       </div>
 
       <div className="terms-link-container">
-        <a
-          href="/terms.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ts & Cs
+        <a href="/terms.html" target="_blank" rel="noopener noreferrer">
+          Ts&Cs
         </a>
       </div>
     </>
