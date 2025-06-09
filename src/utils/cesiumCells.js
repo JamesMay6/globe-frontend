faac49934c407484561ef6967e272e9d6e3ac5f4
James Mason <j91mason6@gmail.com>
2025-06-09 23:52:27 +0100

    geom

--- src/utils/cesiumCells.js
+++ src/utils/cesiumCells.js
@@ -69,12 +69,17 @@ export const drawDeletedCells = (viewer, cells) => {
 export const fetchDeletedCells = async (viewer) => {
   const rect = viewer.camera.computeViewRectangle();
   if (!rect) return;
+
   const minLat = Cesium.Math.toDegrees(rect.south);
   const maxLat = Cesium.Math.toDegrees(rect.north);
   const minLon = Cesium.Math.toDegrees(rect.west);
   const maxLon = Cesium.Math.toDegrees(rect.east);
 
-  const res = await fetch(`${API_URL}/deleted?minLat=${minLat}&maxLat=${maxLat}&minLon=${minLon}&maxLon=${maxLon}`);
+  const cameraHeight = viewer.camera.positionCartographic.height;
+
+  const res = await fetch(
+    `${API_URL}/deleted?minLat=${minLat}&maxLat=${maxLat}&minLon=${minLon}&maxLon=${maxLon}&cameraHeight=${cameraHeight}`
+  );
   const cells = await res.json();
   drawDeletedCells(viewer, cells);
 };
