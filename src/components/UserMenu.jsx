b739ffa66ba5bef75fe04839b6bbdccfee3629b5
James Mason <j91mason6@gmail.com>
2025-06-13 21:11:32 +0100

    css fix:wq

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -146,7 +146,7 @@ export default function UserMenu({
           </div>
 
           <div className="walletLinkMenu">
-            <div className="walletLinkInfo" style={{ marginTop: "0.7rem"}}>
+            <div className="walletLinkInfo" >
               <span>Link DTE Wallet</span>
               <button className="info-button" onClick={() => setWalletInfoModalOpen(true)}>i</button>
             </div>
