afc0c086964a64609193fda00be4cc9c87ada257
James Mason <j91mason6@gmail.com>
2025-06-15 22:06:45 +0100

    update modal css

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,12 +2,9 @@ import "../styles/modalOverlay.css";
 
 export default function WelcomeModal({ onClose }) {
   return (
-    <div className="modalOverlay">
-      <div className="modalContent">
-        <h2 className="modalTitle">Welcome to the Globe</h2>
-        <p className="modalText">
-          Welcome to Delete The Earth! 
-        </p>
+    <div className="modal-overlay">
+      <div className="modal">
+        <h3>Welcome to Delete The Earth! </h3>
         <p>Create a free account and start deleting coorindates around the globe </p>
         <p>Compete against others to be the top of the leaderboard and earn DTE tokens as a reward!</p>
         <p>Enjoy Deleting!</p>
